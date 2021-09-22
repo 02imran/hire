454fbc817468c98c02b552850f44f24aaff1f387
02imran <imran40031@gmail.com>
2021-09-22 10:18:28 +0600

    my work order details page done

--- lib/buyer_job_worker/my_work_order_details/components/my_work_order_details_body.dart
+++ lib/buyer_job_worker/my_work_order_details/components/my_work_order_details_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hire_app/components/custom_button.dart';
+import 'package:hire_app/constants.dart';
 
 class MyWorkOrderDetailsBody extends StatelessWidget {
   const MyWorkOrderDetailsBody({Key? key}) : super(key: key);
@@ -14,11 +16,42 @@ class MyWorkOrderDetailsBody extends StatelessWidget {
             children: [
               SizedBox(height: 20),
               Text('Demo title', style: TextStyle(fontSize: 20)),
-              Container(
-                height: 200,
+              Image.asset(
+                'assets/images/building.png',
+                height: 300,
                 width: double.infinity,
-                child: Image.asset('assets/images/building.png'),
-              )
+              ),
+              SizedBox(height: 20),
+              Text('Work order details:', style: TextStyle(fontSize: 22)),
+              SizedBox(height: 10),
+              Text('Quantity Requred: 5000 yards',
+                  style: TextStyle(fontSize: 16)),
+              SizedBox(height: 10),
+              Text('Other Details:', style: TextStyle(fontSize: 14)),
+              SizedBox(height: 5),
+              Text(
+                  'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,',
+                  style: TextStyle(fontSize: 12)),
+              SizedBox(height: 20),
+              Center(child: Text('9 bids', style: TextStyle(fontSize: 10))),
+              Center(
+                child: CustomeButton(
+                  text: 'Bidders List',
+                  press: () {},
+                  color: kPurpleColor,
+                  left: 80,
+                  right: 80,
+                ),
+              ),
+              Center(
+                child: CustomeButton(
+                  text: 'Close Work Order',
+                  press: () {},
+                  color: kPurpleColor,
+                  left: 60,
+                  right: 60,
+                ),
+              ),
             ],
           ),
         ),
